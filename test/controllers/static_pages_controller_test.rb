25bdc070e34492a2efb5beeb379563b2ee3843e2
Alex Locke <alocke_8181@yahoo.com>
2024-12-03 12:37:22 -0500

    added root -> home

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "root should be home" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | Ruby on Railts Tutorial App"
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
