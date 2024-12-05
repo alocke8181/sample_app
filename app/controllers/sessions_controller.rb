8564f2ee43fa0fee9437a46ea3e2e4ba96c03237
Alex Locke <alocke_8181@yahoo.com>
2024-12-05 15:24:26 -0500

    login integration test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    session = params[:session]
+    user = User.find_by(email: session[:email].downcase)
+    if user && user.authenticate(session[:password])
+      #login
+    else
+      flash[:danger] = 'Invalid email or password'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
