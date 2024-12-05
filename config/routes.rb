6ee401491fe06df27db3f59712069b55047e5343
Alex Locke <alocke_8181@yahoo.com>
2024-12-05 15:13:00 -0500

    login form created

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
 
-  resources :users  
+  resources :users
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
