21b3f504a6fccd57c3b0c34eed15557850004e67
Alex Locke <alocke_8181@yahoo.com>
2024-12-10 14:17:43 -0500

    fixed dropdown not working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session = params[:session]
-    user = User.find_by(email: session[:email].downcase)
-    if user&.authenticate(session[:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      remember user
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email or password'
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -37,4 +37,15 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test 'remembering test' do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies[:remember_token]
+  end
+
+  test 'not remembering test' do
+    log_in_as(@user, remember_me: '1')
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies[:remember_token]
+  end
 end
