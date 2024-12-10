c16bc5affa28718128020ad5cde1c422b3e2f4cf
Alex Locke <alocke_8181@yahoo.com>
2024-12-10 10:38:56 -0500

    logout added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     session = params[:session]
     user = User.find_by(email: session[:email].downcase)
-    if user && user.authenticate(session[:password])
+    if user&.authenticate(session[:password])
       log_in user
       redirect_to user
     else
@@ -15,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -12,9 +12,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
+    assert is_logged_in?
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 
   test 'invalid login' do
@@ -23,6 +31,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     post login_path, params: { session: {email: ' ', password: ' '} }
     assert_template 'sessions/new'
     assert_not flash.empty?
+    assert_not is_logged_in?
     get root_path
     assert flash.empty?
   end
