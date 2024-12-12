b4e4e415ecc62cae9cf0abb2bce035595d9c65d7
Alex Locke <alocke_8181@yahoo.com>
2024-12-12 13:23:26 -0500

    user controller redirects for no login

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,29 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:bob)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+
+  test 'redirects edit for no login' do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test 'redirects update for no login' do
+    patch user_path(@user), params:{
+      user:{
+        name: @user.name,
+        email: @user.email
+      }
+    }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 end
