980dd42275a08600a27fe7d46fef7ac1e6eb7155
Alex Locke <alocke_8181@yahoo.com>
2024-12-17 13:01:44 -0500

    show microposts on a users page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
 User.create!(   
     name: "Example User",
     email: "example@example.com",
-    password: "foobar",
-    password_confirmation: "foobar",
+    password: "password",
+    password_confirmation: "password",
     admin: true,
     activated: true,
     activated_at: Time.zone.now
 )
-
-first_user = User.first
-
-5.times do |x|
-    content = "Test micropost number #{x}"
-    first_user.microposts.build(content: content)
-end
-
 
 99.times do |n|
     name = Faker::Name.name
@@ -38,4 +20,10 @@ end
         activated: true,
         activated_at: Time.zone.now
     )
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+    content = Faker::Lorem.sentence(word_count: 5)
+    users.each {|user| user.microposts.create!(content:content)}
 end
