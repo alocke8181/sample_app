852cb7650c67843399a2c54ef35c4a09dc073958
Alex Locke <alocke_8181@yahoo.com>
2024-12-17 09:25:32 -0500

    activation email tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test 'does not create user on invalid signup' do
     get signup_path
     assert_no_difference 'User.count' do
@@ -16,7 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
-  test 'creates new user on valid signup' do
+  test 'valid signup and activation' do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: {
@@ -28,7 +33,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         }
       }
     end
-    #follow_redirect!
-    #assert_template 'users/show'
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    log_in_as(user)
+    assert_not is_logged_in?
+    get edit_account_activation_path('wrong token', email: user.email)
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: 'wrong@email.com')
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
