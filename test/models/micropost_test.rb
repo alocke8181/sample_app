3437428bd09339c037211362d1fe39de3379e51f
Alex Locke <alocke_8181@yahoo.com>
2024-12-17 12:13:31 -0500

    content validation

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:bob)
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test 'valid micropost' do
+    assert @micropost.valid?
+  end
+
+  test 'user id required' do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test 'content is required' do
+    @micropost.content = ''
+    assert_not @micropost.valid?
+  end
+
+  test 'content limited to 140 chars' do
+    @micropost.content = 'a' * 141
+    assert_not @micropost.valid?
+  end
+
 end
