6c6c6d2cd0f11a08427f2adbd589aa36c704de6d
Alex Locke <alocke_8181@yahoo.com>
2024-12-17 15:16:47 -0500

    microposts require login to create/destroy

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @micropost = microposts(:first)
+  end
+
+  test 'redirect create on no login' do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: {micropost: {content: "no login = no posting!"}}
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'redirect destroy on no login' do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to login_url
+  end
 end
