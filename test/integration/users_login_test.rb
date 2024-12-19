ed142fd72d94fc165da1aacf42d74a0c19138ce7
Alex Locke <alocke_8181@yahoo.com>
2024-12-19 09:31:28 -0500

    updated login test to look for button

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -14,7 +14,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template 'users/show'
     assert is_logged_in?
     assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
+    assert_select "form[action=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
     assert_not is_logged_in?
@@ -23,7 +23,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "form[action=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 
